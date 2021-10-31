e5148496e6bd15b5613fa336ad42b372e9f29ec3
Brian D. Baker <brian.devs.baker@gmail.com>
2021-10-30 23:26:34 -0400

    Added route for creating a new user

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,38 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+// Router for creating a new user
+router.post('/signup', async (req, res) => {
+  try {
+    const alreadyExists = await User.findOne({where: { email: req.body.email }});
+    if (alreadyExists) {
+      return res.status(400).json({message: "That email address is already registered."});
+    }
+
+    const userData = await User.create(req.body);
+
+    if (!userData) {
+      return res
+        .status(400)
+        .json({
+          message: "Failed to create new user. Make sure your password is at least than 8 characters long."
+        });
+    }
+
+    // Create session variables based on the new in user
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      
+      res.json({ user: userData, message: 'You are now registered and logged in!' });
+    });
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// Router for logging in as an existing user
 router.post('/login', async (req, res) => {
   try {
     // Find the user who matches the posted e-mail address
@@ -36,6 +68,7 @@ router.post('/login', async (req, res) => {
   }
 });
 
+//Router for logging out as a user
 router.post('/logout', (req, res) => {
   if (req.session.logged_in) {
     // Remove the session variables
